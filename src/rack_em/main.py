fa27278d755535b207151039ae35fb4f00ccd38c
woodFordR <adam@kaewell.com>
2025-08-14 23:55:18 -0400

    adding test files, server funcs for sending files, libraries for find, match, search, regex

--- src/rack_em/main.py
+++ src/rack_em/main.py
@@ -1,6 +1,8 @@
 # rack_em is a python HTTP server created with a socket server
-
 import argparse
+import os
+import pathlib
+import re
 import socket
 import threading
 
@@ -21,15 +23,37 @@ def find_data_in_req(data):
 
 
 # get the server response
-def get_resp(state, type, body):
+def get_resp(state, type, body, file=False):
+    if file:
+        body_len = os.path.getsize(str(body))
+        body_data = ""
+    else:
+        body_len = len(body)
+        body_data = body
+
     headers = [
         f"HTTP/1.1 {state}",
         f"Content-Type: {type}",
-        f"Content-Length: {len(body)}",
+        f"Content-Length: {body_len}",
         "",
-        body,
+        body_data,
     ]
     default = "\r\n".join(headers)
+    return default
+
+
+# get file from server
+def get_resp_file(file):
+    f = open(file, "rb")
+    # filesize = os.path.getsize(file)
+    body = f.read()
+    f.close()
+    # status = bytes("HTTP/1.1 200 OK\r\n", "utf-8")
+    # headers = bytes(
+    #     f"Content-Type: {type}/r/n",
+    #     f"Content-Length: {str(filesize)}/r/n",
+    # )
+    default = body
     return default
 
 
@@ -39,20 +63,30 @@ def handle_req(socket):
         data = socket.recv(1024).decode()
         if not data:
             return
+        resp_file = None
 
         _, path, _, headers = find_data_in_req(data)
         if path == "/":
-            receive = get_resp("200 OK", "text/plain", "")
+            resp = get_resp("200 OK", "text/plain", "")
         elif path.startswith("/echo/"):
             string = path.split("/echo/")[1]
-            receive = get_resp("200 OK", "text/plain", string)
+            resp = get_resp("200 OK", "text/plain", string)
         elif path == "/user-agent":
             ua = headers.get("User-Agent", "Unknown")
-            receive = get_resp("200 OK", "text/plain", ua)
+            resp = get_resp("200 OK", "text/plain", ua)
+        elif path.startswith("/files/"):
+            file = path.split("/files/")[1]
+            if file in files:
+                resp = get_resp("200 OK", "application/octet-stream", files[file], True)
+                resp_file = get_resp_file(files[file])
+            else:
+                resp = get_resp("404 Not Found", "text/plain", "")
         else:
-            receive = get_resp("404 Not Found", "text/plain", "")
+            resp = get_resp("404 Not Found", "text/plain", "")
 
-        socket.send(receive.encode())
+        socket.send(resp.encode())
+        if resp_file:
+            socket.sendall(resp_file)
     finally:
         socket.close()
 
@@ -64,11 +98,21 @@ def worker(connection, address):
 
 # server application
 def main():
-    parser = argparse.ArgumentParser(description="Test File For Robots.")
-    parser.add_argument("--directory", type=str, help="Directory")
+    parser = argparse.ArgumentParser(description="file for robots.")
+    parser.add_argument("--directory", type=str, help="directory")
     args = parser.parse_args()
+
     global base_directory
     base_directory = args.directory
+
+    global files
+    files = {}
+    pwd_files = [x for x in pathlib.Path(base_directory).glob("*.*")]
+    for x in pwd_files:
+        file_str = str(x)
+        match = re.search(r"\w+.\w+$", file_str)
+        if match:
+            files[match.group()] = x
 
     server = socket.create_server(("localhost", 4221), reuse_port=True)
     server.listen()
