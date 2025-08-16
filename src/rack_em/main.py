8031fb1aa3565e20a8bc819a3b5b83cf3ba8ef7c
woodFordR <adam@kaewell.com>
2025-08-16 18:32:03 -0400

    added persistent connections, refactored sockets to not close

--- src/rack_em/main.py
+++ src/rack_em/main.py
@@ -77,69 +77,74 @@ def get_resp(state, type, body, headers):
             return default
         else:
             default = default + head + "\r\n"
+
     default = default + "\r\n"
     return default.encode()
 
 
 # handler for the request
 def handle_req(socket):
-    try:
-        http_req = socket.recv(1024).decode("utf-8")
+    with socket:
+        while True:
+            http_req = socket.recv(1024).decode("utf-8")
 
-        if not http_req:
-            return
+            if not http_req:
+                break
 
-        crud, path, _, headers, req_body = find_data_in_req(http_req)
+            crud, path, _, headers, req_body = find_data_in_req(http_req)
 
-        if crud == "POST":
-            if path.startswith("/files/"):
-                file = path.split("/files/")[1]
-                f_path = os.path.join(base_directory, file)
-                os.makedirs(os.path.dirname(f_path), exist_ok=True)
-                if req_body:
-                    with open(f_path, "wb") as f:
-                        f.write(req_body.encode("utf-8"))
-                resp = "HTTP/1.1 201 Created\r\n\r\n"
-            else:
-                resp = get_resp("404 Not Found", "text/plain", "", headers)
-        elif crud == "GET":
-            if path == "/":
-                resp = get_resp("200 OK", "text/plain", "", headers)
-            elif path.startswith("/echo/"):
-                string = path.split("/echo/")[1]
-                resp = get_resp("200 OK", "text/plain", string, headers)
-            elif path == "/user-agent":
-                resp = get_resp("200 OK", "text/plain", headers["User-Agent"], headers)
-            elif path.startswith("/files/"):
-                file = path.split("/files/")[1]
-                f_path = os.path.join(base_directory, file)
-                if os.path.exists(f_path):
-                    with open(f_path, "rb") as f:
-                        content = f.read()
-                        resp = f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {os.path.getsize(f_path)}\r\n\r\n{content.decode('utf-8')}"
-                        f.close()
+            if crud == "POST":
+                if path.startswith("/files/"):
+                    file = path.split("/files/")[1]
+                    f_path = os.path.join(base_directory, file)
+                    os.makedirs(os.path.dirname(f_path), exist_ok=True)
+                    if req_body:
+                        with open(f_path, "wb") as f:
+                            f.write(req_body.encode("utf-8"))
+                    resp = "HTTP/1.1 201 Created\r\n\r\n"
+                else:
+                    resp = get_resp("404 Not Found", "text/plain", "", headers)
+            elif crud == "GET":
+                if path == "/":
+                    resp = get_resp("200 OK", "text/plain", "", headers)
+                elif path.startswith("/echo/"):
+                    string = path.split("/echo/")[1]
+                    resp = get_resp("200 OK", "text/plain", string, headers)
+                elif path == "/user-agent":
+                    resp = get_resp(
+                        "200 OK", "text/plain", headers["User-Agent"], headers
+                    )
+                elif path.startswith("/files/"):
+                    file = path.split("/files/")[1]
+                    f_path = os.path.join(base_directory, file)
+                    if os.path.exists(f_path):
+                        with open(f_path, "rb") as f:
+                            content = f.read()
+                            resp = f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {os.path.getsize(f_path)}\r\n\r\n{content.decode('utf-8')}"
+                            f.close()
+                    else:
+                        resp = get_resp("404 Not Found", "text/plain", "", headers)
                 else:
                     resp = get_resp("404 Not Found", "text/plain", "", headers)
             else:
                 resp = get_resp("404 Not Found", "text/plain", "", headers)
-        else:
-            resp = get_resp("404 Not Found", "text/plain", "", headers)
 
-        if isinstance(resp, str):
-            socket.send(resp.encode())
-        else:
-            socket.sendall(resp)
-    finally:
-        socket.close()
+            if isinstance(resp, str):
+                socket.send(resp.encode())
+            else:
+                socket.sendall(resp)
 
 
 def worker(connection, address):
-    print(f"working on {address} ... for the robots ...")
+    print(f"working on {address} ... for the robots ...\n")
     handle_req(connection)
 
 
 # server application
 def main():
+    SERVER = "localhost"
+    PORT = 4221
+
     parser = argparse.ArgumentParser(description="file for robots.")
     parser.add_argument("--directory", type=str, help="directory")
     args = parser.parse_args()
@@ -147,21 +152,25 @@ def main():
     global base_directory
     base_directory = args.directory
 
-    server = socket.create_server(("localhost", 4221), reuse_port=True)
-    server.listen()
-    print("server up up up ... port port port 4221 ...")
+    # server = socket.create_server(("localhost", 4221), reuse_port=True)
+    # server.listen()
 
-    try:
-        while True:
-            print("patiently waiting for that connection ...")
-            conn, loc = server.accept()
-            t = threading.Thread(target=worker, args=(conn, loc))
-            t.start()
-    except KeyboardInterrupt:
-        print("\nserver going going going ...")
-    finally:
-        server.close()
-        print("server down down down ...")
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((SERVER, PORT))
+        s.listen(5)
+        print("server up up up ... port port port 4221 ...\n")
+
+        try:
+            while True:
+                print("patiently waiting for that connection ...\n")
+                conn, loc = s.accept()
+                t = threading.Thread(target=worker, args=(conn, loc))
+                t.start()
+        except KeyboardInterrupt:
+            print("\nserver going going going ...\n")
+        finally:
+            s.close()
+            print("server down down down ...\n")
 
 
 if __name__ == "__main__":
