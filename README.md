a25a804978f45f75d17db40991fb515399fec5cd
woodFordR <adam@kaewell.com>
2025-08-17 01:05:07 -0400

    refacoring methods, naming of methods, cleaning up language of vars

--- README.md
+++ README.md
@@ -8,3 +8,37 @@
  - ```uv sync --dev``` 
  - ```uv run src/rack_em/main.py``` 
 
+---
+
+### curl cli tests
+  - ```curl -v http://localhost:3000```
+  - ```curl -v http://localhost:3000/university```
+  - ```curl -v http://localhost:3000/echo/woodford```
+  - ```curl -v --header "User-Agent: exercise/1.2.3" http://localhost:3000/user-agent```
+  - ```nc localhost 3000``` && ```curl -v localhost:3000```
+  - ```echo -n 'let us hike and camp this weekend' > src/rack_em/data/files/camping``` && 
+    ```curl -i http://localhost:3000/files/camping```
+  - ```curl -i http://localhost:3000/files/my_vacant_file```
+  - ```./your_program.sh --directory **/**/files/```
+  - ```curl -v --data "440000" -H "Content-Type: application/octet-stream" http://localhost:3000/files/file_217```
+  - ```curl -v -H "Accept-Encoding: gzip" http://localhost:3000/echo/tictac```
+  - ```curl -v -H "Accept-Encoding: horrible-encoding" http://localhost:3000/echo/gum```
+  - ```curl -v -H "Accept-Encoding: nonsense-encoding-1, gzip, awful-encoding-2" http://localhost:3000/echo/kentucky```
+  - ```curl -v -H "Accept-Encoding: terrible-encoding-1, nonsense-encoding-2" http://localhost:3000/echo/jersey```
+  - ```curl -v -H "Accept-Encoding: gzip" http://localhost:3000/echo/volleyball | hexdump -C```
+  - ```curl --http1.1 -v http://localhost:3000/echo/strawberry --next http://localhost:3000/user-agent -H "User-Agent: kiwi/peach-kiwi"```
+  - ```curl --http1.1 -v http://localhost:3000/user-agent -H "User-Agent: vanilla/cranberry-lime" --next http://localhost:3000/echo/sushi```
+  - ```curl --http1.1 -v http://localhost:3000/echo/aqua-green --next http://localhost:3000/ -H "Connection: close"```
+---
+
+### response breakdown
+  - > // Status line
+HTTP/1.1  // HTTP version
+200       // Status code
+OK        // Optional reason phrase
+\r\n      // CRLF that marks the end of the status line
+
+  - >// Headers (empty)
+\r\n      // CRLF that marks the end of the headers
+
+  - >// Response body (empty)
--- src/rack_em/main.py
+++ src/rack_em/main.py
@@ -1,4 +1,4 @@
-# rack_em is a python HTTP server created with a socket server
+# rack_em is a python HTTP socket server
 
 import argparse
 import gzip
@@ -6,53 +6,55 @@ import os
 import socket
 import threading
 
+SERVER = "localhost"
+PORT = 4221
+
 
 # request data is parsed
-def find_data_in_req(data):
-    info = data.split("\r\n")
-    begin = info[0]
-    func, loc, version = begin.split(" ")
+def parse_request(data):
+    request_array = data.split("\r\n")
+    request_status_line = request_array[0]
+    method, addr, version = request_status_line.split(" ")
 
-    header_end = data.find("\r\n\r\n")
-    body_start = header_end + 4
+    request_header_end = data.find("\r\n\r\n")
+    request_body_start = request_header_end + 4
 
-    if header_end != -1 and body_start != -1:
-        req_body = data[body_start:-1] + data[-1]
-    else:
-        req_body = None
+    request_body = None
+    if request_header_end != -1 and request_body_start != -1:
+        request_body = data[request_body_start:-1] + data[-1]
 
     headers = {}
-    for line in info[1:]:
-        if line == "":
+    for header in request_array[1:]:
+        if header == "":
             break
-        h, v = line.split(": ", 1)
-        if h == "Accept-Encoding":
-            encodings = v.split(",")
-            for e in encodings:
+        header_key, header_value = header.split(": ", 1)
+        if header_key == "Accept-Encoding":
+            accept_encodings = header_value.split(",")
+            for e in accept_encodings:
                 encoding = e.replace(" ", "")
                 if encoding == "gzip":
                     headers[f"Content-Encoding-{encoding}"] = encoding
                 else:
                     headers[f"Content-Encoding-{encoding}"] = None
             else:
-                headers[h] = v
-        elif h == "User-Agent":
-            headers[h] = v
-        elif h == "Connection" and v == "close":
-            headers[h] = v
+                headers[header_key] = header_value
+        elif header_key == "User-Agent":
+            headers[header_key] = header_value
+        elif header_key == "Connection" and header_value == "close":
+            headers[header_key] = header_value
 
-    return func, loc, version, headers, req_body
+    return method, addr, version, headers, request_body
 
 
 # get the server response
-def get_resp(state, type, body, headers, request_count):
-    new_headers = []
-    new_headers.append(f"HTTP/1.1 {state}")
-    new_headers.append(f"Content-Type: {type}")
+def get_response(state, type, body, headers, request_count):
+    response_headers = []
+    response_headers.append(f"HTTP/1.1 {state}")
+    response_headers.append(f"Content-Type: {type}")
     for key in headers:
         if key.startswith("Content-Encoding"):
             if headers[key] is not None:
-                new_headers.append(f"Content-Encoding: {headers[key]}")
+                response_headers.append(f"Content-Encoding: {headers[key]}")
                 if isinstance(body, str):
                     data = body.encode("utf-8")
                 else:
@@ -65,20 +67,20 @@ def get_resp(state, type, body, headers, request_count):
             continue
         elif key == "Connection":
             if request_count == 2:
-                new_headers.append(f"{key}: {headers[key]}")
+                response_headers.append(f"{key}: {headers[key]}")
             else:
                 continue
         else:
-            new_headers.append(f"{key}: {headers[key]}")
+            response_headers.append(f"{key}: {headers[key]}")
 
     if body:
-        new_headers.append(f"Content-Length: {len(body)}\r\n")
-        new_headers.append(body)
+        response_headers.append(f"Content-Length: {len(body)}\r\n")
+        response_headers.append(body)
     elif isinstance(body, str):
-        new_headers.append("\r\n")
+        response_headers.append("\r\n")
 
     default = b""
-    for head in new_headers:
+    for head in response_headers:
         if isinstance(head, bytes):
             return default + head
         else:
@@ -88,104 +90,118 @@ def get_resp(state, type, body, headers, request_count):
 
 
 # handler for the request
-def handle_req(socket):
+def handle_request(socket):
     request_count = 0
-    with socket:
+
+    with socket as s:
         while True:
-            http_req = socket.recv(1024).decode("utf-8")
+            request = s.recv(1024).decode("utf-8")
             request_count += 1
 
-            if not http_req:
+            if not request:
                 break
 
-            crud, path, _, headers, req_body = find_data_in_req(http_req)
+            method, request_url, _, request_headers, request_body = parse_request(
+                request
+            )
 
-            if crud == "POST":
-                if path.startswith("/files/"):
-                    file = path.split("/files/")[1]
-                    f_path = os.path.join(base_directory, file)
+            if method == "POST":
+                if request_url.startswith("/files/"):
+                    file = request_url.split("/files/")[1]
+                    f_path = os.path.join(base_dir, file)
                     os.makedirs(os.path.dirname(f_path), exist_ok=True)
-                    if req_body:
+                    if request_body:
                         with open(f_path, "wb") as f:
-                            f.write(req_body.encode("utf-8"))
-                    resp = "HTTP/1.1 201 Created\r\n\r\n"
+                            f.write(request_body.encode("utf-8"))
+                    response = "HTTP/1.1 201 Created\r\n\r\n"
                 else:
-                    resp = get_resp(
-                        "404 Not Found", "text/plain", "", headers, request_count
-                    )
-            elif crud == "GET":
-                if path == "/":
-                    resp = get_resp("200 OK", "text/plain", "", headers, request_count)
-                elif path.startswith("/echo/"):
-                    string = path.split("/echo/")[1]
-                    resp = get_resp(
-                        "200 OK", "text/plain", string, headers, request_count
-                    )
-                elif path == "/user-agent":
-                    resp = get_resp(
-                        "200 OK",
+                    response = get_response(
+                        "404 Not Found",
                         "text/plain",
-                        headers["User-Agent"],
-                        headers,
+                        "",
+                        request_headers,
                         request_count,
                     )
-                elif path.startswith("/files/"):
-                    file = path.split("/files/")[1]
-                    f_path = os.path.join(base_directory, file)
+            elif method == "GET":
+                if request_url == "/":
+                    response = get_response(
+                        "200 OK", "text/plain", "", request_headers, request_count
+                    )
+                elif request_url.startswith("/echo/"):
+                    string = request_url.split("/echo/")[1]
+                    response = get_response(
+                        "200 OK", "text/plain", string, request_headers, request_count
+                    )
+                elif request_url == "/user-agent":
+                    response = get_response(
+                        "200 OK",
+                        "text/plain",
+                        request_headers["User-Agent"],
+                        request_headers,
+                        request_count,
+                    )
+                elif request_url.startswith("/files/"):
+                    file = request_url.split("/files/")[1]
+                    f_path = os.path.join(base_dir, file)
                     if os.path.exists(f_path):
                         with open(f_path, "rb") as f:
                             content = f.read()
-                            resp = f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {os.path.getsize(f_path)}\r\n\r\n{content.decode('utf-8')}"
+                            response = f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {os.path.getsize(f_path)}\r\n\r\n{content.decode('utf-8')}"
                             f.close()
                     else:
-                        resp = get_resp(
-                            "404 Not Found", "text/plain", "", headers, request_count
+                        response = get_response(
+                            "404 Not Found",
+                            "text/plain",
+                            "",
+                            request_headers,
+                            request_count,
                         )
                 else:
-                    resp = get_resp(
-                        "404 Not Found", "text/plain", "", headers, request_count
+                    response = get_response(
+                        "404 Not Found",
+                        "text/plain",
+                        "",
+                        request_headers,
+                        request_count,
                     )
             else:
-                resp = get_resp(
-                    "404 Not Found", "text/plain", "", headers, request_count
+                response = get_response(
+                    "404 Not Found", "text/plain", "", request_headers, request_count
                 )
 
-            if isinstance(resp, str):
-                socket.send(resp.encode())
+            if isinstance(response, str):
+                s.send(response.encode())
             else:
-                socket.sendall(resp)
+                s.sendall(response)
 
-            if "Connection" in headers.keys() and request_count == 2:
-                if headers["Connection"] == "close":
-                    socket.shutdown()
+            if "Connection" in request_headers.keys() and request_count == 2:
+                if request_headers["Connection"] == "close":
+                    s.shutdown()
 
 
-def worker(connection, address):
-    print(f"working on {address} ... for the robots ...\n")
-    handle_req(connection)
+def request_worker(connection, address):
+    print(f"working {address} for the robots ...\n")
+    handle_request(connection)
 
 
 # server application
 def main():
-    SERVER = "localhost"
-    PORT = 4221
-
-    parser = argparse.ArgumentParser(description="file for robots.")
-    parser.add_argument("--directory", type=str, help="directory")
+    parser = argparse.ArgumentParser(description="a python http server for robots")
+    parser.add_argument("--dir", type=str, help="directory for server files")
     args = parser.parse_args()
 
-    global base_directory
-    base_directory = args.directory
+    global base_dir
+    base_dir = args.directory
 
     with socket.create_server((SERVER, PORT), reuse_port=True) as s:
         s.listen(5)
-        print(f"server up up up ... port port port {PORT} ...\n")
+        print(f"serving up {SERVER} @ port {PORT} ...\n")
 
         try:
             while True:
-                print("patiently waiting for that connection ...\n")
-                conn, loc = s.accept()
-                t = threading.Thread(target=worker, args=(conn, loc))
+                print("waiting for connection ...\n")
+                conn, addr = s.accept()
+                t = threading.Thread(target=request_worker, args=(conn, addr))
                 t.start()
         except KeyboardInterrupt:
             print("\nserver going going going ...\n")
